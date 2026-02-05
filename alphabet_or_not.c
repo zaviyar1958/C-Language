8b06dd49442382c421c3473a193c264221f8cf42
mohammad-vaqqas <mohammadvaqqas5@gmail.com>
2026-02-05 18:16:04 +0530

    program to find alphabet or not

---
+++ alphabet_or_not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character : ");
+    scanf("%c",&ch);
+    if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+    {
+        printf("The given character is an alphabet.");
+    }
+    else
+    {
+        printf("The given character is not an alphabet.");
+    }
+    return 0;
+}
