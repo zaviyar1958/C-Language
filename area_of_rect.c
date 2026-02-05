9f3afab5823e1417d7e96555d43b973efb143235
mohammad-vaqqas <mohammadvaqqas5@gmail.com>
2026-02-05 18:07:36 +0530

    program to find area of rect

---
+++ area_of_rect.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,a;
+    printf("Enter length of rectangle : ");
+    scanf("%d",&x);
+    printf("Enter width of rectangle : ");
+    scanf("%d",&y);
+    a=x*y;
+    printf("Area of rectangle is %d",a);
+    return 0;
+}
