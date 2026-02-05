9059a097679e0d5ebf93638aca873003a5feae43
mohammad-vaqqas <mohammadvaqqas5@gmail.com>
2026-02-05 18:12:39 +0530

    program to print weeks using switch

---
+++ week_printing.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number (0-6) : ");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 0 :
+        printf("Monday");
+        break;
+        case 1 :
+        printf("Tuesday");
+        break;
+        case 2 :
+        printf("Wednesday");
+        break;
+        case 3 :
+        printf("Thursday");
+        break;
+        case 4 :
+        printf("Friday");
+        break;
+        case 5 :
+        printf("Saturday");
+        break;
+        case 6 :
+        printf("Sunday");
+        break;
+        default :
+        printf("Invalid input");
+    }
+    return 0;
+}
